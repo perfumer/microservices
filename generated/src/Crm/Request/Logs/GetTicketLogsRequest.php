7651c959c148a242a006803a3e52be46c797d6b5
torbayevnurbek1992@gmail.com <Kegli1992>
2021-01-24 03:48:05 +0600

    new api from crm

--- generated/src/Crm/Request/Logs/GetTicketLogsRequest.php
+++ generated/src/Crm/Request/Logs/GetTicketLogsRequest.php
@@ -12,6 +12,11 @@ class GetTicketLogsRequest extends \Perfumer\Microservices\Request
     /**
      * @var string
      */
+    public $type = null;
+
+    /**
+     * @var string
+     */
     public $text = null;
 
     /**
@@ -28,6 +33,11 @@ class GetTicketLogsRequest extends \Perfumer\Microservices\Request
      * @var string
      */
     public $status = null;
+
+    /**
+     * @var string
+     */
+    public $close_status = null;
 
     /**
      * @var string
@@ -52,10 +62,12 @@ class GetTicketLogsRequest extends \Perfumer\Microservices\Request
     public function __construct()
     {
         $this->ticket = new \Perfumer\Microservices\Undefined();
+        $this->type = new \Perfumer\Microservices\Undefined();
         $this->text = new \Perfumer\Microservices\Undefined();
         $this->user = new \Perfumer\Microservices\Undefined();
         $this->code = new \Perfumer\Microservices\Undefined();
         $this->status = new \Perfumer\Microservices\Undefined();
+        $this->close_status = new \Perfumer\Microservices\Undefined();
         $this->form = new \Perfumer\Microservices\Undefined();
         $this->limit = new \Perfumer\Microservices\Undefined();
         $this->offset = new \Perfumer\Microservices\Undefined();
---
+++ src/Crm/Response/Ticket/CloseTicketResponse.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Perfumer\Microservices\Crm\Response\Ticket;
+
+use Perfumer\Microservices\Response;
+
+class CloseTicketResponse extends Response
+{
+}
