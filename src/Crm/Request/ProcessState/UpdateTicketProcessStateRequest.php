81fd51afe0831185a0454cf91b505d7a3d304954
nurbek <torbayevnurbek1992@gmail.com>
2021-01-26 16:40:19 +0600

    ticket state

---
+++ src/Crm/Request/ProcessState/UpdateTicketProcessStateRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Perfumer\Microservices\Crm\Request\ProcessState;
+
+class UpdateTicketProcessStateRequest extends \Generated\Perfumer\Microservices\Crm\Request\ProcessState\UpdateTicketProcessStateRequest
+{
+
+
+}
