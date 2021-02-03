d4efc5700648fef047d5e68002a8fbda0f50cbe8
torbayevnurbek1992@gmail.com <Kegli1992>
2021-02-03 10:20:02 +0600

    update form field api

--- generated/src/Events/Request/Events/GetEventsRequest.php
+++ generated/src/Events/Request/Events/GetEventsRequest.php
@@ -89,6 +89,16 @@ class GetEventsRequest extends \Perfumer\Microservices\Request
      */
     public $count = null;
 
+    /**
+     * @var string
+     */
+    public $order_field = null;
+
+    /**
+     * @var string
+     */
+    public $order_direction = null;
+
     public function __construct()
     {
         $this->organizer_id = new \Perfumer\Microservices\Undefined();
@@ -108,5 +118,7 @@ class GetEventsRequest extends \Perfumer\Microservices\Request
         $this->limit = new \Perfumer\Microservices\Undefined();
         $this->offset = new \Perfumer\Microservices\Undefined();
         $this->count = new \Perfumer\Microservices\Undefined();
+        $this->order_field = new \Perfumer\Microservices\Undefined();
+        $this->order_direction = new \Perfumer\Microservices\Undefined();
     }
 }
