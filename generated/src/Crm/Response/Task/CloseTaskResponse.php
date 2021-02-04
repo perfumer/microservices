7437964d83c069b8a8acd3d11dd5d7797662f58e
torbayevnurbek1992@gmail.com <Kegli1992>
2021-02-04 19:04:53 +0600

    close task

---
+++ generated/src/Crm/Response/Task/CloseTaskResponse.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Generated\Perfumer\Microservices\Crm\Response\Task;
+
+class CloseTaskResponse extends \Perfumer\Microservices\Response
+{
+    /**
+     * @var array
+     */
+    public $task = [
+    ];
+}
