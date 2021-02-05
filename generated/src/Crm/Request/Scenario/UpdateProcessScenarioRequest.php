307d22d4cd4012ce194d8aeb562ed23575c7df8a
torbayevnurbek1992@gmail.com <Kegli1992>
2021-02-05 12:25:11 +0600

    deleted old code

--- generated/src/Crm/Request/Scenario/UpdateProcessScenarioRequest.php
+++ generated/src/Crm/Request/Scenario/UpdateProcessScenarioRequest.php
@@ -24,11 +24,17 @@ class UpdateProcessScenarioRequest extends \Perfumer\Microservices\Request
      */
     public $to_state = null;
 
+    /**
+     * @var string
+     */
+    public $webhook = null;
+
     public function __construct()
     {
         $this->id = new \Perfumer\Microservices\Undefined();
         $this->code = new \Perfumer\Microservices\Undefined();
         $this->from_state = new \Perfumer\Microservices\Undefined();
         $this->to_state = new \Perfumer\Microservices\Undefined();
+        $this->webhook = new \Perfumer\Microservices\Undefined();
     }
 }
