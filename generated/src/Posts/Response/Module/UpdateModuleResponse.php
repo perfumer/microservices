ac4499d59984d326d145f96311a78426cc572632
Abylay <abyllight@gmailcom>
2021-02-09 12:39:11 +0600

    posts module update

---
+++ generated/src/Posts/Response/Module/UpdateModuleResponse.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Generated\Perfumer\Microservices\Posts\Response\Module;
+
+class UpdateModuleResponse extends \Perfumer\Microservices\Response
+{
+    /**
+     * @var array
+     */
+    public $module = [
+    ];
+}
