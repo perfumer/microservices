7a4b7c996562bdbbe4a1c2f27d7a1f386a9da9eb
nurbek <torbayevnurbek1992@gmail.com>
2021-02-19 14:29:09 +0600

    ncanode apis 2

--- src/Ncanode/Ncanode.php
+++ src/Ncanode/Ncanode.php
@@ -4,6 +4,8 @@ namespace Perfumer\Microservices\Ncanode;
 
 class Ncanode extends \Generated\Perfumer\Microservices\Ncanode\Ncanode
 {
-
-
+    public function __construct($host)
+    {
+        $this->host = $host;
+    }
 }
