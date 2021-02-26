32682a9066c69bc994cace0f636b00f67538f5b1
m.zhimailov <meiram.zhimailov@yandex.ru>
2021-02-26 14:28:01 +0600

    add nb_invites

--- src/Contract/Events/Events.php
+++ src/Contract/Events/Events.php
@@ -22,7 +22,7 @@ use Perfumer\Microservices\Annotation\GetModels;
  * @GetModel(microservice="events", model="participant", fields={"id.int"})
  * @GetModels(microservice="events", model="events", fields={"organizer_id", "locale", "module_id.int", "author_id", "title", "image",
  *     "description", "opened_at_from", "opened_at_to", "closed_at_from", "closed_at_to", "apply_from_from",
- *     "apply_from_to", "apply_till_from", "apply_till_to", "published_at", "nb_invites_per_participant"})
+ *     "apply_from_to", "apply_till_from", "apply_till_to", "published_at", "nb_invites_per_participant", "nb_invites"})
  *
  * @SaveModel(microservice="events", model="participant", fields={"event_id", "customer_id", "nb_invites"})
  * @UpdateModel(microservice="events", model="participant", fields={"id.int", "event_id", "customer_id", "nb_invites"})
@@ -30,7 +30,7 @@ use Perfumer\Microservices\Annotation\GetModels;
  *
  * @GetModel(microservice="events", model="event", fields={"id.int", "locale"})
  * @SaveModel(microservice="events", model="event", fields={"id.int", "modules.array", "organizer_id", "locale", "author_id", "is_registration_open.bool", "title", "image",
- *     "description", "html", "json", "format",  "opened_at", "closed_at", "apply_from", "apply_till", "published_at", "nb_invites_per_participant"})
+ *     "description", "html", "json", "format",  "opened_at", "closed_at", "apply_from", "apply_till", "published_at", "nb_invites_per_participant", "nb_invites"})
  * @DeleteModel(microservice="events", model="event", fields={"id.int"})
  * @SaveModel(microservice="events", model="event", fields={"id.int"}, url="/event/reject", action="reject")
  * @SaveModel(microservice="events", model="event", fields={"id.int"}, url="/event/publish", action="publish")
