56f1e8e60563ba7003c17320ef7792d0a4440ac6
mgt95 <meiram.zhimailov@yandex.ru>
2021-03-08 23:03:47 +0600

    split save and update

--- src/Contract/Events/Events.php
+++ src/Contract/Events/Events.php
@@ -13,7 +13,8 @@ use Perfumer\Microservices\Annotation\GetModels;
 /**
  * @GetModel(microservice="events", model="module", fields={"id", "code"})
  * @GetMeshModels(microservice="events", model="modules", fields={"name", "code", "description", "is_archived.bool"})
- * @SaveMeshModel(microservice="events", model="module", fields={"id", "code", "name", "description", "is_archived.bool"})
+ * @SaveMeshModel(microservice="events", model="module", fields={"code", "name", "description", "is_archived.bool"})
+ * @UpdateModel(microservice="events", model="module", fields={"id", "code", "name", "description", "is_archived.bool"})
  * @DeleteModel(microservice="events", model="module", fields={"id", "code"})
  * @SaveModel(microservice="events", model="module", fields={"id.int"}, url="/module/archive", action="archive")
  * @SaveModel(microservice="events", model="module", fields={"id.int"}, url="/module/archive", action="unarchive", request_method="delete")
