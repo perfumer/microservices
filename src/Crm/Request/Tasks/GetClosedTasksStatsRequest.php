9980a6dc54866cddb77295cc4d50a2144e932ff5
m.zhimailov <meiram.zhimailov@yandex.ru>
2021-03-16 19:14:42 +0600

    add closed_tasks

---
+++ src/Crm/Request/Tasks/GetClosedTasksStatsRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Perfumer\Microservices\Crm\Request\Tasks;
+
+class GetClosedTasksStatsRequest extends \Generated\Perfumer\Microservices\Crm\Request\Tasks\GetClosedTasksStatsRequest
+{
+
+
+}
