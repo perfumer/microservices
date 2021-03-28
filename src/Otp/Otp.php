9a06d023ffb74dcbb92f789afc95c909966a26ad
mgt95 <meiram.zhimailov@yandex.ru>
2021-03-28 20:42:12 +0600

    add constructor and conf services.php

--- src/Otp/Otp.php
+++ src/Otp/Otp.php
@@ -4,6 +4,8 @@ namespace Perfumer\Microservices\Otp;
 
 class Otp extends \Generated\Perfumer\Microservices\Otp\Otp
 {
-
-
+    public function __construct($host)
+    {
+        $this->host = $host;
+    }
 }
