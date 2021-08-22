80b7d0d9098754ad967005341679b729dcbf1454
torbayevnurbek1992@gmail.com <Kegli1992>
2021-08-23 01:07:17 +0600

    Api-Segment header

--- src/Ncanode/Ncanode.php
+++ src/Ncanode/Ncanode.php
@@ -4,8 +4,31 @@ namespace Perfumer\Microservices\Ncanode;
 
 class Ncanode extends \Generated\Perfumer\Microservices\Ncanode\Ncanode
 {
-    public function __construct($host)
+    /**
+     * @var string
+     */
+    private $segment;
+
+    public function __construct($host, $segment)
     {
         $this->host = $host;
+
+        $this->segment = $segment;
+
+        if ($segment !== null && $segment !== '') {
+            $this->addHeader('Api-Segment', $segment);
+        }
+    }
+
+    public function getSegment(): ?string
+    {
+        return $this->segment;
+    }
+
+    public function setSegment(string $segment): void
+    {
+        $this->segment = $segment;
+
+        $this->addHeader('Api-Segment', $segment);
     }
 }
