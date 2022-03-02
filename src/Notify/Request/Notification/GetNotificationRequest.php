00ec80b283f9c4123b46638065718266b3f5bdbb
ilyas <ilyasus@list.ru>
2022-03-02 15:56:11 +0600

    GetNotification

---
+++ src/Notify/Request/Notification/GetNotificationRequest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Perfumer\Microservices\Notify\Request\Notification;
+
+class GetNotificationRequest extends \Generated\Perfumer\Microservices\Notify\Request\Notification\GetNotificationRequest
+{
+}
