46ec3db117d12b4dbf2ba4df5f5f04939ff89a1d
ilyas <ilyasus@list.ru>
2022-06-06 11:30:37 +0600

    Postgrest

--- src/Postgrest/Postgrest.php
+++ src/Postgrest/Postgrest.php
@@ -12,6 +12,11 @@ class Postgrest extends \Generated\Perfumer\Microservices\Postgrest\Postgrest
         if ($guzzle_response) {
             $response->_http_status_code = $guzzle_response->getStatusCode();
             $response->_raw = $guzzle_response->getBody()->getContents();
+
+            $response->_status = $guzzle_response->getStatusCode() > 0 && $guzzle_response->getStatusCode() < 400;
+
+            $decoded_response = json_decode($response->_raw, true);
+            $response->_content = (array) $decoded_response;
         }
 
         return $response;
