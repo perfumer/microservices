c9626da11cbce263873aaa76a3d18c5d51a016ed
ilyas <ilyasus@list.ru>
2022-06-30 11:03:19 +0600

    User bin oprg_name

--- generated/src/Crm/Request/User/UpdateUserRequest.php
+++ generated/src/Crm/Request/User/UpdateUserRequest.php
@@ -73,6 +73,16 @@ class UpdateUserRequest extends \Perfumer\Microservices\Request
     /**
      * @var string
      */
+    public $bin = null;
+
+    /**
+     * @var string
+     */
+    public $org_name = null;
+
+    /**
+     * @var string
+     */
     public $code = null;
 
     /**
@@ -140,6 +150,8 @@ class UpdateUserRequest extends \Perfumer\Microservices\Request
         $this->email = new \Perfumer\Microservices\Undefined();
         $this->phone = new \Perfumer\Microservices\Undefined();
         $this->iin = new \Perfumer\Microservices\Undefined();
+        $this->bin = new \Perfumer\Microservices\Undefined();
+        $this->org_name = new \Perfumer\Microservices\Undefined();
         $this->code = new \Perfumer\Microservices\Undefined();
         $this->password_hash = new \Perfumer\Microservices\Undefined();
         $this->password = new \Perfumer\Microservices\Undefined();
@@ -193,6 +205,12 @@ class UpdateUserRequest extends \Perfumer\Microservices\Request
         }
         if (!$this->iin instanceof \Perfumer\Microservices\Undefined) {
             $array['iin'] = $this->iin;
+        }
+        if (!$this->bin instanceof \Perfumer\Microservices\Undefined) {
+            $array['bin'] = $this->bin;
+        }
+        if (!$this->org_name instanceof \Perfumer\Microservices\Undefined) {
+            $array['org_name'] = $this->org_name;
         }
         if (!$this->code instanceof \Perfumer\Microservices\Undefined) {
             $array['code'] = $this->code;
