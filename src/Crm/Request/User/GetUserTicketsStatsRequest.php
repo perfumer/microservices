eec3f7df9dbfba4331972ac94a12c28e283b1df2
Ilyas <ilyasus@list.ru>
2023-01-05 13:04:21 +0600

    Tickets stats

---
+++ src/Crm/Request/User/GetUserTicketsStatsRequest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Perfumer\Microservices\Crm\Request\User;
+
+class GetUserTicketsStatsRequest extends \Generated\Perfumer\Microservices\Crm\Request\User\GetUserTicketsStatsRequest
+{
+}
---
+++ src/Crm/Response/User/GetUserTicketsStatsResponse.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Perfumer\Microservices\Crm\Response\User;
+
+class GetUserTicketsStatsResponse extends \Generated\Perfumer\Microservices\Crm\Response\User\GetUserTicketsStatsResponse
+{
+}
