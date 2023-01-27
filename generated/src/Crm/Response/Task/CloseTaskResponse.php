2b37ba3500ffbcbcc991319b144080b02052ad49
Ilyas <mehmatovec@gmail.com>
2023-01-27 13:26:57 +0600

    Release v5.3.3

--- generated/src/Crm/Response/Task/CloseTaskResponse.php
+++ generated/src/Crm/Response/Task/CloseTaskResponse.php
@@ -5,6 +5,11 @@ namespace Generated\Perfumer\Microservices\Crm\Response\Task;
 class CloseTaskResponse extends \Perfumer\Microservices\Response
 {
     /**
+     * @var string
+     */
+    public $next_task_id = null;
+
+    /**
      * @var array
      */
     public $task = [
