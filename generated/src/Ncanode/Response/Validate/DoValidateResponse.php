1b68e3c5c2c24a0f3d89f963ae518816e5de01b9
ilyas <ilyasus@list.ru>
2024-05-07 23:57:21 +0600

    New fields for doValdate

--- generated/src/Ncanode/Response/Validate/DoValidateResponse.php
+++ generated/src/Ncanode/Response/Validate/DoValidateResponse.php
@@ -5,6 +5,51 @@ namespace Generated\Perfumer\Microservices\Ncanode\Response\Validate;
 class DoValidateResponse extends \Perfumer\Microservices\Response
 {
     /**
+     * @var string
+     */
+    public $iin;
+
+    /**
+     * @var string
+     */
+    public $bin;
+
+    /**
+     * @var string
+     */
+    public $first_name;
+
+    /**
+     * @var string
+     */
+    public $last_name;
+
+    /**
+     * @var string
+     */
+    public $mid_name;
+
+    /**
+     * @var string
+     */
+    public $org_name;
+
+    /**
+     * @var string
+     */
+    public $gender;
+
+    /**
+     * @var string
+     */
+    public $email;
+
+    /**
+     * @var string
+     */
+    public $birthday;
+
+    /**
      * @var array
      */
     public $validate = [
